6059625d7cb5b9513869bb12e271d5af9129757f
Ricardo Figueiredo <rickoalex@gmail.com>
2026-02-06 17:04:06 +0000

    Set up Express server with webhook and health routes

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-app.listen(process.env.PORT || 3000);
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/webhook", (req, res) => {
+  console.log("Incoming Twilio payload:");
+  console.log(req.body);
+  res.send("ok");
+});
+
+app.get("/health", (_, res) => {
+  res.send("ok");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running");
+});
